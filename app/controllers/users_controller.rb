24a69866063dad889665a4c91277399fdd3ec219
DoLLaRs6252 <punvarattaya6252@gmail.com>
2024-09-05 21:58:38 +0700

    add func edit,update in Userscontroller to patch user data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,13 +12,26 @@ class UsersController < ApplicationController
     end
   end
 
-  def show
+  def index
     @user = User.all
   end
 
   def destroy
     @user = User.find(params[:id])
     @user.destroy
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to index_path
+    else
+      render :edit
+    end
   end
 
   private
