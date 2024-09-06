699dc837eb1115e6c3f4dfb3d0161d78b487ee2c
DoLLaRs6252 <punvarattaya6252@gmail.com>
2024-09-06 09:49:33 +0700

    refactor routes and edit path link to go between page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get '/new', to: 'users#new'
-  get '/index', to: 'users#index'
-  # get 'users/create'
-  post 'users' ,to: 'users#create'
+  # get '/new', to: 'users#new'
+  # get '/index', to: 'users#index'
+  # post 'users' ,to: 'users#create' 
+  # delete 'users/:id',to:'users#destroy', as: :user_delete 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
