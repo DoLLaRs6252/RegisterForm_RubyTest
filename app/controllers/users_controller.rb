d0932b3e8244b43d0731a6261f85e6ad07118cbe
DoLLaRs6252 <punvarattaya6252@gmail.com>
2024-09-10 15:45:59 +0700

    refactor routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
   def destroy
     @user = User.find(params[:id])
     @user.destroy
-    redirect_to user_path
+    redirect_to user_delete_path
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
-  # get '/new', to: 'users#new'
-  # get '/index', to: 'users#index'
-  # post 'users' ,to: 'users#create' 
-  # delete 'users/:id',to:'users#destroy', as: :user_delete 
+  get 'users/new', to: 'users#new',as: :user_new
+  post 'users/new' ,to: 'users#create',as: :user_create
+  get 'users', to: 'users#index',as: :users
+  get 'users/:id', to: 'users#edit', as: :user_edit
+  patch 'users/:id', to: 'users#update', as: :user_update
+  delete 'users/:id',to:'users#destroy', as: :user_delete 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
-  resources :users, only: [:index, :new, :create, :edit, :update , :patch,:destroy]
+  # resources :users, only: [:index, :new, :create, :edit, :update , :patch,:destroy]
   root "users#new"
 end
